fcaf1f7ec010ae260a537a4d34487c6d8152ab19
hyrdbyrd <IRuss23@yandex.ru>
2022-05-12 00:37:44 +0300

    active uploading

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ const { promisify } = require('util');
 const { resolve } = require('path');
 
 const writeFile = promisify(fs.writeFile);
+const readFile = promisify(fs.readFile);
 
 const wait = t => new Promise(r => setTimeout(r, t));
 const log = (...args) => {
@@ -54,7 +55,8 @@ console.clear();
 
   await page.goto('https://yandex.ru/maps/geo/respublika_krym/1444278541/?ll=34.526191%2C45.226951&z=7.26');
 
-  const places = ['пляж альбатрос в севастополе'];
+  // const places = require('./receive_img.json');
+  const places = ['Пляж MRIYA RESORT & SPA', 'Hellowa', 'Алушта']
   const result = [];
 
   const errored = [];
@@ -62,15 +64,17 @@ console.clear();
   let idx = 0;
   for (const place of places) {
     try {
+      await page.click('.small-search-form-view__icon._type_close').catch(() => undefined);
+
        // Search
       await page.click('.search-form-view__input');
       await page.type('.search-form-view__input .input__control', place);
       await page.click('.small-search-form-view__button');
 
-      await wait(1500);
+      await wait(2000);
 
       // Getting coordinates
-      await page.click('.search-business-snippet-view__title'); // done
+      await page.click('.search-business-snippet-view__title');
       await wait(500);
       await page.click('.business-contacts-view__address-link');
       await wait(500);
@@ -84,17 +88,23 @@ console.clear();
         coords: coords.replace(' ', '').split(',')
       });
 
+      const dataJson = require('./data.json') || [];
+      await writeFile(resolve(__dirname, 'data.json'), JSON.stringify(dataJson.concat(result), null, 4));
+
       log(
         `${place} обработан ✅\n` +
         `${++idx} из ${places.length}`
       );
     } catch (e) {
+      errored.push(place);
+
+      const erroredJson = require('./errored.json') || [];
+      await writeFile(resolve(__dirname, 'errored.json'), JSON.stringify(erroredJson.concat(errored), null, 4));
+
       log(
         `${place} не обработан ⚠️ \n` +
         `${++idx} из ${places.length}`
       );
-
-      errored.push(place);
     }
   }
 
@@ -104,7 +114,6 @@ console.clear();
     `${errored.length} не обработано ⚠️ \n`
   );
 
-  await writeFile(resolve(__dirname, 'data.json'), JSON.stringify(result, null, 4));
   await writeFile(resolve(__dirname, 'errored.json'), JSON.stringify(errored, null, 4));
 
   await wait(5000);
