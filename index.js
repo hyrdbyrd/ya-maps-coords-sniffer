550ae452bbc7be29cf7f7805da2ecabdada27389
hyrdbyrd <IRuss23@yandex.ru>
2022-05-12 19:33:11 +0300

    better code wins

--- index.js
+++ index.js
@@ -34,37 +34,60 @@ const fs = require('fs');
 const { promisify } = require('util');
 const { resolve } = require('path');
 
+const doNothing = () => undefined;
+
 const writeFile = promisify(fs.writeFile);
-const readFile = promisify(fs.readFile);
+
+const _require = (path, elseData) => new Promise((resolve, reject) => {
+  try {
+    resolve(require(path));
+  } catch (e) {
+    resolve(elseData);
+  }
+});
 
 const wait = t => new Promise(r => setTimeout(r, t));
+
 const log = (...args) => {
   console.clear();
   console.log(...args);
 };
 
+const writeJSONTo = (path, data) =>
+  writeFile(resolve(__dirname, path), JSON.stringify(data, null, 4));
+
 console.clear();
 
 (async () => {
+  /** @ SETTUP @ */
+
   const browser = await puppeteer.launch({
+    /** Если надо дебажить, подрубить */
     // headless: false,
     ignoreDefaultArgs: ['--disable-extensions'],
     executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe'
   });
   const page = await browser.newPage();
-
   await page.goto('https://yandex.ru/maps/geo/respublika_krym/1444278541/?ll=34.526191%2C45.226951&z=7.26');
 
-  // const places = require('./receive_img.json');
-  const places = ['Пляж MRIYA RESORT & SPA', 'Hellowa', 'Алушта']
+  const places = await _require('./input.json', false);
+  if (!places) log('Заполните input.json для работы скрипта');
+
   const result = [];
 
   const errored = [];
 
+  /** @ CLEANING @ */
+
+  writeJSONTo('data.json', []);
+  writeJSONTo('errored.json', []);
+
+  /** @ MAIN @ */
+
   let idx = 0;
   for (const place of places) {
     try {
-      await page.click('.small-search-form-view__icon._type_close').catch(() => undefined);
+      await page.click('.small-search-form-view__icon._type_close').catch(doNothing);
 
        // Search
       await page.click('.search-form-view__input');
@@ -74,7 +97,7 @@ console.clear();
       await wait(2000);
 
       // Getting coordinates
-      await page.click('.search-business-snippet-view__title');
+      await page.click('.search-business-snippet-view__title').catch(doNothing);
       await wait(500);
       await page.click('.business-contacts-view__address-link');
       await wait(500);
@@ -88,8 +111,8 @@ console.clear();
         coords: coords.replace(' ', '').split(',')
       });
 
-      const dataJson = require('./data.json') || [];
-      await writeFile(resolve(__dirname, 'data.json'), JSON.stringify(dataJson.concat(result), null, 4));
+      const dataJson = await _require('./data.json', []);
+      await writeJSONTo('data.json', dataJson.concat(result));
 
       log(
         `${place} обработан ✅\n` +
@@ -98,8 +121,8 @@ console.clear();
     } catch (e) {
       errored.push(place);
 
-      const erroredJson = require('./errored.json') || [];
-      await writeFile(resolve(__dirname, 'errored.json'), JSON.stringify(erroredJson.concat(errored), null, 4));
+      const erroredJson = await _require('./errored.json', []);
+      await writeJSONTo('errored.json', erroredJson.concat(errored));
 
       log(
         `${place} не обработан ⚠️ \n` +
@@ -108,13 +131,13 @@ console.clear();
     }
   }
 
-  log(
-    result,
-    `\n${result.length} обработано ✅\n`,
-    `${errored.length} не обработано ⚠️ \n`
-  );
+  /** @ ENDING @ */
 
-  await writeFile(resolve(__dirname, 'errored.json'), JSON.stringify(errored, null, 4));
+  log(
+    'Работа скрипта завершена\n',
+    `${result.length} обработано ✅\n`,
+    `${errored.length} не обработано ⚠️\n`,
+  );
 
   await wait(5000);
 
